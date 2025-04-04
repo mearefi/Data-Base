375cc19ab5d76c09f71c30c2bc8446f71ca898a3
mehrdadarefi <me.arefi1319@gmail.com>
2025-04-04 17:44:50 +0330

    adding the methods to database class

--- prj/src/db/Database.java
+++ prj/src/db/Database.java
@@ -8,5 +8,38 @@ public class Database {
 
     private Database() {}
 
+    public static void add(Entity entity) {
+        entity.id = nextId;
+        nextId++;
+        entities.add(entity);
+    }
 
+    public static Entity get(int id) {
+        for (Entity entity : entities) {
+            if (entity.id == id) {
+                return entity;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == id) {
+                entities.remove(i);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void update(Entity entity) {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == entity.id) {
+                entities.set(i, entity);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(entity.id);
+    }
 }
