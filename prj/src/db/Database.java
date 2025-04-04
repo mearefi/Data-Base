16eca72952abb0438d7bbc72fb4bb3b38beee005
mehrdadarefi <me.arefi1319@gmail.com>
2025-04-04 17:44:01 +0330

    creating database class and arraylist in it

---
+++ prj/src/db/Database.java
@@ -0,0 +1,12 @@
+package db;
+
+import java.util.ArrayList;
+
+public class Database {
+    private static final ArrayList<Entity> entities = new ArrayList<>();
+    private static int nextId = 1;
+
+    private Database() {}
+
+
+}
