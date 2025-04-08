6f8efce04729896302129cd36caa8875b43f0524
mehrdadarefi <me.arefi1319@gmail.com>
2025-04-08 15:23:18 +0330

    adding new code to test the main method

--- prj/src/Main.java
+++ prj/src/Main.java
@@ -1,13 +1,13 @@
 import example.Human;
+import example.HumanValidator;
 import db.*;
 import db.exception.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Human ali = new Human("Ali");
-        int assignedId = Database.add(ali);
-        ali.id = assignedId;
-        Human aliFromTheDatabase = (Human) Database.get(ali.id);
-        System.out.println("ali's name in the database: " + aliFromTheDatabase.name);
+    public static void main(String[] args) throws InvalidEntityException {
+        Database.registerValidator(Human.HUMAN_ENTITY_CODE, new HumanValidator());
+
+        Human ali = new Human("Ali", -10);
+        Database.add(ali);
     }
 }
